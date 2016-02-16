aebcb1b78a1972e6e2b4aa3fabfea4ee01e9d607
camshaft <bytheway.cameron@gmail.com>
2016-02-16 16:05:09 -0700

    add package info

--- mix.exs
+++ mix.exs
@@ -3,11 +3,13 @@ defmodule HTMLBuilder.Mixfile do
 
   def project do
     [app: :html_builder,
+     description: "generate html in elixir with simple data structures",
      version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,]
   end
 
   def application do
@@ -16,5 +18,12 @@ defmodule HTMLBuilder.Mixfile do
 
   defp deps do
     []
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Cameron Bytheway"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/camshaft/html_builder"}]
   end
 end
